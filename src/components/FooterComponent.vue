c3004cd599babcd223544a77107161d4773756f5
Nathan <nschpro@gmail.com>
2022-06-09 16:56:33 +0200

    footer without responsive

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -21,10 +21,31 @@
                 </ul>
             </div>
         </section>
+        <section>
+            <div class="bar"></div>
+            <p><router-link class="mentions" to="/mentions-legales">Mentions légales</router-link> - © {{currentYear}} IFS73</p>
+            <div class="bar"></div>
+        </section>
     </footer>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  }
+};
+</script>
+
 <style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Righteous&display=swap');
+
+    *{
+        font-family: 'Poppins', sans-serif;
+    }
+
     footer{
         background: white;
     }
@@ -36,12 +57,43 @@
         flex-wrap: wrap;
     }
 
+    img{
+        width: 275px;
+        height: auto;
+    }
+
+    h2{
+        margin-bottom: 1rem;
+        font-family: 'Righteous', cursive;
+    }
+
     li{
         list-style: none;
         text-decoration: none;
+        padding: .2rem 0;
     }
 
     .router{
         text-decoration: none;
+        color: black;
+    }
+
+    .router:hover{
+        color: RGBA(0,0,0,0.70);
+    }
+
+    .mentions{
+        text-decoration: none;
+        color: #7E7E7E;
+    }
+
+    .bar{
+        width: clamp(10%, 35%, 40%);
+        height: 1px;
+        background: black;
+    }
+
+    p{
+        color: #7E7E7E;
     }
 </style>
