6c213e0a30b90ac72ef401e2a8eea65f44f2d110
Nathan <nschpro@gmail.com>
2022-06-09 17:27:05 +0200

    Init html

--- src/components/AccueilComponent.vue
+++ src/components/AccueilComponent.vue
@@ -1,6 +1,51 @@
 <template>
     <main>
-        <h1>Accueil</h1>
+        <!-- slider -->
+        <h1></h1>
+        <h2></h2>
+        <section>
+            <div>
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h2></h2>
+                <router-link to="/formations"></router-link>
+            </div>
+            <img src="" alt="">
+        </section>
+        <section>
+            <img src="" alt="">
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+            <img src="" alt="">
+        </section>
+        <h2></h2>
+        <section>
+            <!-- carousel -->
+        </section>
     </main>
 </template>
 
