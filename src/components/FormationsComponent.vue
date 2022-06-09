4bbb98e76dfe7cd0fb23e6d858c4d37db22e11fb
Raphael Hernandez <raphhernandez1220@gmail.com>
2022-06-09 19:20:12 +0200

    Html completed

--- src/components/FormationsComponent.vue
+++ src/components/FormationsComponent.vue
@@ -1,6 +1,62 @@
 <template>
     <main>
-        <h1>Formations</h1>
+        <h1>Decouvrer nos formations</h1>
+        <section>
+            <div>
+                <img src="/assets/icons8-microsoft-word-2019.svg" alt="logo microsoft word">
+                <button>Découvrir</button>
+            </div>
+            <div>
+                <img src="/assets/icons8-microsoft-excel-2019.svg" alt="logo microsoft excel">
+                <button>Découvrir</button>               
+            </div>
+            <div>
+                <img src="/assets/icons8-microsoft-powerpoint-2019.svg" alt="logo microsoft powerpoint">
+                <button>Découvrir</button>
+            </div>
+        </section>
+        
+        <div>
+            <h3>Vous souhaitez acquérir des compétences et améliorer votre maîtrise des logiciels de bureautique ?</h3>
+            <p>
+                La maitrise de la bureautique renforce votre productivité et votre efficacité professionnelle.
+                Et la formation professionnelle continue vous permet de développer ces compétences, profitez-en !
+                
+                IFS vous propose des formations aux logiciels Microsoft Word, Microsoft Excel, Microsoft Powerpoint,
+                entièrement sur mesure, ​qui s'adaptent complètement à vos besoins spécifiques.
+                
+                Les sessions de formation sont organisées en distanciel ou à Aix-les-Bains ou Chambéry,
+                individuellement ou en groupe.
+            </p>
+        </div>
+        
+        <section>
+            <img src="/assets/02.png" alt="illutration femme formation">
+            <div>
+                <h2>Contactez nous !</h2>
+                <p><a href="">Contact</a></p>
+            </div>
+        </section>
+       
+        <section>
+            <div>
+                <h3>Une formation pour tous !</h3>
+                <p>
+                    Que vous soyez salarié, entrepreneur ou demandeur d’emploi,
+                    toute personne souhaitant approfondir ses connaissances
+                    ou au contraire souhaitant découvrir Word, Excel, Powerpoint,
+                    dans un contexte professionnel ou personnel. 
+                </p>
+            </div>
+            <div>
+                <h3>Comment financer sa formation ?</h3>
+                <p> 
+                    Informatique Formation Sécurité est enregistré en tant qu'organisme de formation professionnelle 
+                    sous le numéro 84 73 0197573 auprès de la DREETS de la région Auvergne-Rhône-Alpes 
+                    (cet enregistrement ne vaut pas agrément de l'Etat). 
+                </p>
+            </div>
+        </section>
     </main>
 </template>
 
